b4beb5c34227165d19de673c53a0d41fe3fbfa31
athagi <at.hgt.dev@gmail.com>
2019-03-30 02:08:20 +0900

    tell deference

--- main.go
+++ main.go
@@ -1,21 +1,34 @@
-// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package main
 
-import "github.com/athagi/maru-hi/cmd"
+import (
+	"flag"
+	"fmt"
+	"image"
+	"os"
+
+	_ "image/gif"
+	_ "image/jpeg"
+	_ "image/png"
+)
 
 func main() {
-	cmd.Execute()
+	var (
+		path = flag.String("p", "", "path to image")
+	)
+	flag.Parse()
+	fmt.Println(*path)
+
+	f, err := os.Open(*path)
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	defer f.Close()
+	conf, format, err := image.DecodeConfig(f)
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	fmt.Println(format)
+	fmt.Println(conf)
 }
